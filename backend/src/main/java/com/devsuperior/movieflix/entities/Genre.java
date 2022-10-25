4504dad71521295ae8ea7449ebc5a801c7b548e6
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-10-25 11:49:35 -0300

    Movie

--- backend/src/main/java/com/devsuperior/movieflix/entities/Genre.java
+++ backend/src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -1,12 +1,15 @@
 package com.devsuperior.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +22,9 @@ public class Genre implements Serializable {
 	private Long id;
 	private String name;
 
+	@OneToMany(mappedBy = "genre")
+	private List<Movie> movies = new ArrayList<>();
+	
 	public Genre() {
 	}
 
